17f88480a703b349830025a6bd73a229e53aff8a
zkw <zkw@gmail.com>
2018-08-16 10:09:02 +0800

    加入显示天气功能

---
+++ app/src/main/java/com/example/zkw/coolweather/gson/NOW.java
@@ -0,0 +1,17 @@
+package com.example.zkw.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class NOW {
+
+    @SerializedName("tmp")
+    public String temperature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+        @SerializedName("txt")
+        public String info;
+    }
+}
